131f2e99c37def4b424044a085babe83c6ae6543
Kevin Mcmackin <kevinmcmackin@Kevins-MacBook-Pro.local>
2023-05-09 23:14:41 -0300

    fixed footer and nav bar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -26,8 +26,38 @@ const Nav = () => {
         })
     }, [])
 
+    // to make Nav dissapear when at footer
+    const [isAtEnd, setIsAtEnd] = useState(false);
+    const lastSectionRef = useRef(null);
+
+    useEffect(() => {
+        const sections = document.querySelectorAll('section');
+        lastSectionRef.current = sections[sections.length - 1];
+
+        const handleScroll = () => {
+            const windowHeight = window.innerHeight;
+            const documentHeight = document.body.clientHeight;
+            const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+            setIsAtEnd(scrollTop + windowHeight >= documentHeight);
+        
+            sections.forEach((section) => {
+                const sectionTop = section.offsetTop;
+                const sectionHeight = section.clientHeight;
+                if (scrollTop >= sectionTop - sectionHeight / 2) {
+                    setActiveNav(`#${section.id}`);
+                }
+            });
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <nav>
+        <nav style={{ display: isAtEnd ? 'none' : null }}>
             <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
             <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
             <a href="#skills" onClick={() => setActiveNav('#skills')} className={activeNav === '#skills' ? 'active' : ''}><MdChecklistRtl /></a>
